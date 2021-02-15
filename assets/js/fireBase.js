8fc8713cf7fdd780637b7846378b2e54059dd1b8
Me <abc234@protonmail.com>
2021-02-16 09:45:28 +1100

    Basic Auth with fireBase, about to re-factor fireBase. OK

---
+++ assets/js/fireBase.js
@@ -0,0 +1,21 @@
+// var mainApp = {};
+
+// (function(){
+//     var firebase = app_fireBase;
+//     var uid = null;
+//     firebase.auth().onAuthStateChanged(function(user){
+//         if(user){
+//                 //User is signed in.
+//                 uid = user.id;
+//             } else {
+//                 uid = null;
+//                 window.location.replace("login.md")
+//             }
+//         });
+
+//     function logOut(){
+//         firebase.auth().signOut();
+//     }
+
+//     mainApp.logOut = logOut;
+// })()
